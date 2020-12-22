4c0088ec3efc7e386d303060a778a67bbefff357
farhatamiine <farhatamiine@gmail.com>
2020-12-22 11:57:19 +0100

    exporting data to excel

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const puppeteer = require('puppeteer');
 
 function delay(time) {
@@ -11,15 +12,14 @@ async function getAllProductRef(LINKS) {
         headless: false,
         defaultViewport: { isLandscape: true, width: 1366, height: 768 }
     });
+    const products = [];
     const page = await browser.newPage();
     await page.goto(LINKS, { waitUntil: 'networkidle2' })
     await page.select("select#ctl00_cpholder_ctl00_ChildItems_PageSizectl_dlPageSize", "9999")
-    console.log("Done 1");
+    console.log(chalk.red("Waiting for filter..."));
     await delay(10000);
-    console.log("Done 2");
-    const products = [];
+    console.log(chalk.green("Done"));
     const option = await page.$$eval("span#spnShortDescription", a => a.length);
-    console.log(option);
     for (let index = 0; index < option; index++) {
         let selector = index < 9 ? "#ctl00_cpholder_ctl00_ChildItems_rptchilditems_ctl0" + index + "_Hk2" : "#ctl00_cpholder_ctl00_ChildItems_rptchilditems_ctl" + index + "_Hk2"
         const ref = await page.$$eval(selector, r => r.map(s => s.href));
@@ -30,5 +30,3 @@ async function getAllProductRef(LINKS) {
         return el != null;
     });
 };
-
-console.table(getAllProductRef("https://www.disway.com/productdetails.aspx?id=20000034&itemno=MASTER_TAB_SMARTPHON").then(e => console.log(e)))
